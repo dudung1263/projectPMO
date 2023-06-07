9ed64d64d3a1709f985b1141345294411fd59df7
Nunubsw <titoditya456@gmail.com>
2023-06-08 00:57:30 +0700

    menu bar

---
+++ app/src/main/java/e/amil/e_amil/data_zakat.java
@@ -0,0 +1,14 @@
+package e.amil.e_amil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class data_zakat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_zakat);
+    }
+}
--- app/src/main/java/e/amil/e_amil/home.java
+++ app/src/main/java/e/amil/e_amil/home.java
@@ -1,5 +1,6 @@
 package e.amil.e_amil;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,35 +8,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link home#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class home extends Fragment {
+public class home extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    Button btnzakat, btninfaq, btnsodakoh;
+    View view ;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     public home() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment home.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static home newInstance(String param1, String param2) {
         home fragment = new home();
@@ -58,7 +45,35 @@ public class home extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        btnzakat = view.findViewById(R.id.datazakat);
+        btninfaq = view.findViewById(R.id.datainfaq);
+        btnsodakoh = view.findViewById(R.id.datasodakoh);
+
+        btnzakat.setOnClickListener(this);
+        btninfaq.setOnClickListener(this);
+        btnsodakoh.setOnClickListener(this);
+        return view;
     }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.datazakat:
+                getActivity().startActivity(new Intent(getActivity(), data_zakat.class));
+                break;
+            case R.id.datainfaq:
+                getActivity().startActivity(new Intent(getActivity(), data_infaq.class));
+                break;
+            case R.id.datasodakoh:
+                getActivity().startActivity(new Intent(getActivity(), data_sodakoh.class));
+                break;
+
+        }
+
+    }
+    @Override
+    public void onDestroyView(){super.onDestroyView();}
 }
