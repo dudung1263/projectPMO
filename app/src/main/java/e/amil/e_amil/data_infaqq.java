f8d119656a7511c746dc6d893fa62b1a0c82f040
Nurmabsw <nunubsw@gmail.com>
2023-06-08 19:42:23 +0700

    actifity Infaq

---
+++ app/src/main/java/e/amil/e_amil/data_infaqq.java
@@ -0,0 +1,14 @@
+package e.amil.e_amil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class data_infaqq extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_infaqq);
+    }
+}
