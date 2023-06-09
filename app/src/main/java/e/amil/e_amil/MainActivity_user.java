76fbe4cd07c234edbaf2ffffdc694e873958c91e
Nurmabsw <nunubsw@gmail.com>
2023-06-09 10:31:11 +0700

    Actifity User

---
+++ app/src/main/java/e/amil/e_amil/MainActivity_user.java
@@ -0,0 +1,14 @@
+package e.amil.e_amil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_user extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_user);
+    }
+}
