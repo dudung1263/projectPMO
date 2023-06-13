2e92f02fd2989a8b38420d31cc386bd1b8b066a2
ginda <gindadwi144@gmail.com>
2023-06-13 22:45:32 +0700

    image slider dokumen

--- app/src/main/java/e/amil/e_amil/dokumen.java
+++ app/src/main/java/e/amil/e_amil/dokumen.java
@@ -5,10 +5,15 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,8 +30,18 @@ public class dokumen extends Fragment implements View.OnClickListener {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private int[] imageResources = {R.drawable.slidezakat, R.drawable.slideinfaq, R.drawable.slidesodakoh};
 
     Button Btndatazakat_data,  Btndatainfaq_data, Btndatasodakoh_data;
+
+    private ImageView imageView;
+    private int currentPage = 0;
+    private Timer timer;
+
+    private final long DELAY_MS = 500; // Delay in milliseconds before task is to be executed
+    private final long PERIOD_MS = 3000; // Time interval in milliseconds between successive task executions.
+
+
 
     View view;
 
@@ -67,9 +82,38 @@ public class dokumen extends Fragment implements View.OnClickListener {
         Btndatasodakoh_data.setOnClickListener(this);
         Btndatainfaq_data.setOnClickListener(this);
 
+
+        imageView = view.findViewById(R.id.imageView);
+
+        // Auto start image slider
+        autoStartImageSlider();
+
+
         return view;
 
     }
+
+
+    private void autoStartImageSlider() {
+        final Handler handler = new Handler();
+        final Runnable update = new Runnable() {
+            public void run() {
+                if (currentPage == imageResources.length) {
+                    currentPage = 0;
+                }
+                imageView.setImageResource(imageResources[currentPage++]);
+            }
+        };
+
+        timer = new Timer(); // This will create a new Thread
+        timer.schedule(new TimerTask() { // task to be scheduled
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        }, DELAY_MS, PERIOD_MS);
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -86,6 +130,7 @@ public class dokumen extends Fragment implements View.OnClickListener {
                 break;
         }
     }
+
 
 
 
