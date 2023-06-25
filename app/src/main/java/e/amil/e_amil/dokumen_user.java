1f6e709330cf9d3951907cce746b2d13d4d25484
ginda <gindadwi144@gmail.com>
2023-06-25 15:56:27 +0700

    cardview new

--- app/src/main/java/e/amil/e_amil/dokumen_user.java
+++ app/src/main/java/e/amil/e_amil/dokumen_user.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,14 +20,28 @@ import android.view.ViewGroup;
  */
 public class dokumen_user extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private ImageView imageView;
+    private int[] imageResources = {R.drawable.img_14, R.drawable.img_16, R.drawable.img_12};
+    private final long DELAY_MS =8; // Delay in milliseconds before task is to be executed
+    private final long PERIOD_MS = 3000; // Time interval in milliseconds between successive task executions.
+
+
+
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
+
+
+    private int currentPage = 0;
+    private Timer timer;
+
+
+    View view;
+
 
     public dokumen_user() {
         // Required empty public constructor
@@ -59,6 +78,34 @@ public class dokumen_user extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dokumen_user, container, false);
+        view = inflater.inflate(R.layout.fragment_dokumen_user, container, false);
+
+        // Auto start image slider
+
+        imageView = view.findViewById(R.id.image_dokumenuser);
+        autoStartImageSlider();
+        return view;
+
+
+    }
+
+    private void autoStartImageSlider() {
+        final Handler handler = new Handler();
+        final Runnable update = new Runnable() {
+            public void run() {
+                if (currentPage == imageResources.length) {
+                    currentPage = 0;
+                }
+                imageView.setImageResource(imageResources[currentPage++]);
+            }
+        };
+
+        timer = new Timer(); // This will create a new Thread
+        timer.schedule(new TimerTask() { // task to be scheduled
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        }, DELAY_MS, PERIOD_MS);
     }
 }
