60d0067d3a7ceda58472c34618de496e51b0d6d9
ginda <gindadwi144@gmail.com>
2023-06-26 11:01:19 +0700

    cardview new new2

--- app/src/main/java/e/amil/e_amil/dokumen_user.java
+++ app/src/main/java/e/amil/e_amil/dokumen_user.java
@@ -1,7 +1,9 @@
 package e.amil.e_amil;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
@@ -10,8 +12,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,7 +39,7 @@ public class dokumen_user extends Fragment {
     private final long DELAY_MS =8; // Delay in milliseconds before task is to be executed
     private final long PERIOD_MS = 3000; // Time interval in milliseconds between successive task executions.
 
-
+    private BarChart barChart;
 
 
     private static final String ARG_PARAM1 = "param1";
@@ -38,6 +52,7 @@ public class dokumen_user extends Fragment {
 
     private int currentPage = 0;
     private Timer timer;
+
 
 
     View view;
@@ -80,6 +95,12 @@ public class dokumen_user extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_dokumen_user, container, false);
 
+        //Barchart
+        barChart = view.findViewById(R.id.barChart);
+
+        setupBarChart();
+        loadDataFromFirebase();
+
         // Auto start image slider
 
         imageView = view.findViewById(R.id.image_dokumenuser);
@@ -87,6 +108,56 @@ public class dokumen_user extends Fragment {
         return view;
 
 
+    }
+
+    private void setupBarChart() {
+        // Mengatur deskripsi
+        Description description = new Description();
+        description.setText("Contoh Grafik Batang");
+        barChart.setDescription(description);
+
+        // Mengatur animasi
+        barChart.animateY(1000);
+
+        // Mengatur sumbu X
+        // ...
+
+        // Mengatur sumbu Y
+        // ...
+    }
+
+
+    private void loadDataFromFirebase() {
+        FirebaseDatabase.getInstance().getReference().child("Zakat").child("Infak")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        List<BarEntry> entries = new ArrayList<>();
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            // Ambil nilai dari Firebase
+                            int value = snapshot.getValue(Integer.class);
+
+                            // Tambahkan ke daftar entri untuk grafik batang
+                            entries.add(new BarEntry(entries.size() + 1, value));
+                        }
+
+                        // Buat data set untuk grafik batang
+                        BarDataSet dataSet = new BarDataSet(entries, "Data Set");
+                        dataSet.setColor(Color.GREEN);
+
+                        // Buat objek data untuk grafik batang
+                        BarData barData = new BarData(dataSet);
+
+                        // Atur data ke grafik batang
+                        barChart.setData(barData);
+                        barChart.invalidate(); // Perbarui tampilan grafik batang
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        // Penanganan kesalahan saat memuat data dari Firebase
+                    }
+                });
     }
 
     private void autoStartImageSlider() {
