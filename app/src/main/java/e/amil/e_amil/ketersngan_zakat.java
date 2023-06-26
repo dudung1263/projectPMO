1f228111126552e9cce8b488a66c34c8a3d9925e
ginda <gindadwi144@gmail.com>
2023-06-26 11:58:53 +0700

    cardview new new2

---
+++ app/src/main/java/e/amil/e_amil/ketersngan_zakat.java
@@ -0,0 +1,14 @@
+package e.amil.e_amil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ketersngan_zakat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ketersngan_zakat);
+    }
+}
