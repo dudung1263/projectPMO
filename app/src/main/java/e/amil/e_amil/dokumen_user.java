c0c4d763d213822b297a7554e582e6a93f3da5ae
Nunubsw <titoditya456@gmail.com>
2023-06-26 21:36:14 +0700

    perbaikan tampilan zakatdata user

--- app/src/main/java/e/amil/e_amil/dokumen_user.java
+++ app/src/main/java/e/amil/e_amil/dokumen_user.java
@@ -1,7 +1,6 @@
 package e.amil.e_amil;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
@@ -9,22 +8,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -40,7 +26,6 @@ public class dokumen_user extends Fragment {
     private final long DELAY_MS = 8; // Delay in milliseconds before task is to be executed
     private final long PERIOD_MS = 3000; // Time interval in milliseconds between successive task executions.
 
-    private BarChart barChart;
 
 
     private static final String ARG_PARAM1 = "param1";
@@ -54,7 +39,7 @@ public class dokumen_user extends Fragment {
     private int currentPage = 0;
     private Timer timer;
 
-    private CardView gambarzakat, gambarinfak;
+    private CardView gambarzakat, gambarinfak, jam;
 
 
     View view;
@@ -98,8 +83,8 @@ public class dokumen_user extends Fragment {
         view = inflater.inflate(R.layout.fragment_dokumen_user, container, false);
 
         gambarzakat = view.findViewById(R.id.datazakat_user);
-        //Barchart
-        barChart = view.findViewById(R.id.barChart);
+        jam = view.findViewById(R.id.jadwalsholat_dokumen);
+
 
         gambarzakat.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,8 +94,17 @@ public class dokumen_user extends Fragment {
             }
         });
 
-        setupBarChart();
-        loadDataFromFirebase();
+        jam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(requireContext(), jadwalsholat.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
 
         // Auto start image slider
 
@@ -118,59 +112,6 @@ public class dokumen_user extends Fragment {
         autoStartImageSlider();
         return view;
     }
-
-
-
-    private void setupBarChart() {
-        // Mengatur deskripsi
-        Description description = new Description();
-        description.setText("Contoh Grafik Batang");
-        barChart.setDescription(description);
-
-        // Mengatur animasi
-        barChart.animateY(1000);
-
-        // Mengatur sumbu X
-        // ...
-
-        // Mengatur sumbu Y
-        // ...
-    }
-
-
-    private void loadDataFromFirebase() {
-        FirebaseDatabase.getInstance().getReference().child("Zakat")
-                .addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        List<BarEntry> entries = new ArrayList<>();
-                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                            // Ambil nilai dari Firebase
-                            int value = snapshot.getValue(Integer.class);
-
-                            // Tambahkan ke daftar entri untuk grafik batang
-                            entries.add(new BarEntry(entries.size() + 1, value));
-                        }
-
-                        // Buat data set untuk grafik batang
-                        BarDataSet dataSet = new BarDataSet(entries, "Data Set");
-                        dataSet.setColor(Color.GREEN);
-
-                        // Buat objek data untuk grafik batang
-                        BarData barData = new BarData(dataSet);
-
-                        // Atur data ke grafik batang
-                        barChart.setData(barData);
-                        barChart.invalidate(); // Perbarui tampilan grafik batang
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                        // Penanganan kesalahan saat memuat data dari Firebase
-                    }
-                });
-    }
-
     private void autoStartImageSlider() {
         final Handler handler = new Handler();
         final Runnable update = new Runnable() {
