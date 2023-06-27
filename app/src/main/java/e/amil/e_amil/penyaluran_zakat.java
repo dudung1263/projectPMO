c981357bcf8d1c3052c05003bc38f13c99734815
Nunubsw <titoditya456@gmail.com>
2023-06-27 12:20:25 +0700

    perbaikan home admin

---
+++ app/src/main/java/e/amil/e_amil/penyaluran_zakat.java
@@ -0,0 +1,14 @@
+package e.amil.e_amil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class penyaluran_zakat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_penyaluran_zakat);
+    }
+}
