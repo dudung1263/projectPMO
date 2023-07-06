4e74281e4086cb434293b2bbacf0e6a2004681f6
Nurmabsw <nunubsw@gmail.com>
2023-07-06 11:09:20 +0700

    Perbaikan Keseluruhan

--- app/src/main/java/e/amil/e_amil/data_zakat.java
+++ app/src/main/java/e/amil/e_amil/data_zakat.java
@@ -9,7 +9,6 @@ import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -24,13 +23,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.LoadAdError;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.ads.interstitial.InterstitialAd;
-import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -81,32 +74,7 @@ public class data_zakat extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_zakat);
 
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {}
-        });
-        AdRequest adRequest = new AdRequest.Builder().build();
 
-        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest,
-                new InterstitialAdLoadCallback() {
-
-                    private String TAG;
-
-                    @Override
-                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
-                        // The mInterstitialAd reference will be null until
-                        // an ad is loaded.
-                        mInterstitialAd = interstitialAd;
-                        Log.i(TAG, "onAdLoaded");
-                    }
-
-                    @Override
-                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
-                        // Handle the error
-                        Log.d(TAG, loadAdError.toString());
-                        mInterstitialAd = null;
-                    }
-                });
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
@@ -145,9 +113,7 @@ public class data_zakat extends AppCompatActivity {
         simpan_zakat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mInterstitialAd != null) {
-                    mInterstitialAd.show(data_zakat.this);
-                } else if (rb == null){
+                 if (rb == null){
                     getrjeniszakat = null;
                 } else {
                     getrjeniszakat = rb.getText().toString();
--- app/src/main/java/e/amil/e_amil/penyaluran_infaq.java
+++ app/src/main/java/e/amil/e_amil/penyaluran_infaq.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class penyaluran_infaq extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
